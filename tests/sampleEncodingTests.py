8daa428391f02c4bc95c19fd1aa8023eec849fbb
Tracie Perez <tracie.perez@mavs.uta.edu>
2013-07-18 10:53:10 -0500

    Updating sample encoding script.
    Removing verbose debugging output and specifically saving the potential H matrix candidates (as they are identified and updated) to be used after the loop.

--- tests/sampleEncodingTests.py
+++ tests/sampleEncodingTests.py
@@ -10,12 +10,12 @@ from numpy.random import random_integers
 # not full rank. Using getSystematicGmatrix to get a version that is
 # full rank, then sending it on to the encoding algorithm 
 
-# H = regularLDPCcodeConstructor(1920,4,6)
-H = regularLDPCcodeConstructor(20,3,4)
+H = regularLDPCcodeConstructor(1920,4,6)
+# H = regularLDPCcodeConstructor(20,3,4)
 print 'Origiinal H.shape:', H.shape
-printArrayToFile(H,'H_20_3_4_original.txt')
+printArrayToFile(H,'H_1920_4_6_original.txt')
 [G, newH] = getSystematicGmatrix(H)
-printArrayToFile(G,'G_from_20_3_4_H_matrix.txt')
+printArrayToFile(G,'G_from_1920_4_6_H_matrix.txt')
 print 'linalg.matrix_rank(newH):', linalg.matrix_rank(newH)
 
 # Hp = newH[0:newH.shape[0],0:newH.shape[0]]
@@ -43,8 +43,6 @@ for index in arange(numIterations):
 		E = betterH[t:t+gap,0:t]
 		A = betterH[0:t,t:t+gap]
 		C = betterH[t:t+gap,t:t+gap]
-		B = betterH[0:t,t+gap:n]
-		D = betterH[t:t+gap,t+gap:n]
 		invTmod2array = invMod2(T)
 		temp1  = dot(E,invTmod2array) % 2
 		temp2  = dot(temp1,A) % 2
@@ -61,55 +59,53 @@ for index in arange(numIterations):
 				# phi is nonsingular, so this is our new candidate
 				print 'Found betterH woohoo!'
 				flagFoundBetterH = 1
+
+				finalH = betterH
+				finalGap = gap
+				final_t = t
  	else:
  		print 'Gap:', gap, 'is not larger than curent g:', g
 
 if flagFoundBetterH: 
-	print 'New H matrix has gap g:', gap, 'and t =', t
-	print 'T:\n', T, '\n', T.shape
-	print 'E:\n', E, '\n', E.shape
-	print 'A:\n', A, '\n', A.shape
-	print 'C:\n', C, '\n', C.shape
-	print 'B:\n', B, '\n', B.shape
-	print 'D:\n', D, '\n', D.shape
+	print 'New H matrix has gap g:', finalGap, 'and t =', final_t
 else: 
 	print '\n\nDid not find betterH! We have to quit here...\n\n'
 
 ############ this is all real-time encoding #########################
 if flagFoundBetterH:
-	n = newH.shape[1]
-	k = n - newH.shape[0] 
+	n = finalH.shape[1]
+	k = n - finalH.shape[0]  
 	s = random_integers(0,1,k).reshape(k,1)
-	print 's, filled with k=', k, 'information bits:\n', s
+	print 's, filled with k=', k, 'random information bits:\n'
 	print 's.shape:', s.shape
 
+	T = finalH[0:final_t, 0:final_t]
+	E = finalH[final_t:final_t+finalGap,0:final_t] #
+	A = finalH[0:final_t,final_t:final_t+finalGap] #
+	B = finalH[0:final_t,final_t+finalGap:n] #
+	D = finalH[final_t:final_t+finalGap,final_t+finalGap:n] #
+	invTmod2array = invMod2(T) #
+
 	# compute p1 (this method has lowest complexity)
-	print 'invPhi.shape:', invPhi.shape
 	a = dot(B,s) % 2
-	print 'a.shape:', a.shape
 	b = dot(invTmod2array,a) % 2
-	print 'b.shape:', b.shape
 	c = dot(E,b) % 2
-	print 'c.shape:', c.shape
 	d = dot(D,s) % 2
-	print 'd.shape:', d.shape
 	e = d + c % 2
-	print 'e.shape:', e.shape
 	p2 = dot(invPhi,e) % 2
-	print 'p2.shape:', p2.shape
-	print 'p2:\n', p2
 
 	# compute p2 (this method has lowest complexity)
 	a = dot(A, p2) % 2
 	b = dot(B, s) % 2
 	c = a + b % 2
 	p1 = dot(invTmod2array,c) % 2
-	print 'p1:\n', p1
 
 	# concatenate to get codeword x
 	x = vstack((p1, p2, s))
-	print 'Codeword x:\n', x
 
 	# verify:
-	print 'This should be zeros:\n', dot(betterH,x) % 2
-	
+	testArray = dot(finalH,x) % 2
+	if testArray.any():
+		print '\n\nCodeword did not pass parity check!!!\n'
+	else: 
+		print '\n\nCodeword passed partiy check.\n' 
