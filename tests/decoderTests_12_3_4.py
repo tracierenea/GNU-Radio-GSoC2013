a9ca6d7961a09a3af6febe5bb3e63457825e242d
Tracie Perez <tracie.perez@mavs.uta.edu>
2013-07-25 17:39:40 -0500

    Updating this script to use newly written functions.
    This script will generate an H message with user-specified properties, then generate codewords from randomly generated information bits. Then, these codewords are used to test the bit flip decoder. Currenty, for each codeword, it tests one bit flip (flipping every bit in the codeword) an a maximum of 10000 cases of 2 randomly chosen bits flipped.

--- tests/decoderTests_12_3_4.py
+++ tests/decoderTests_12_3_4.py
@@ -1,103 +1,110 @@
 #!/usr/bin/python
-from LDPCfunctions import *
 from numpy import *
+from LDPCencoding import RichardsonUrbankeEncoder
+from LDPCdecoding import *
+from LDPCpreprocessing import *
 from numpy.random import random_integers
+import sys
 
-# this (12,3,4) H is from Example A.12 in Modern Coding Theory.
-# It's full rank
-H = array([[0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0],  # 1
-	       [1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0],  # 2
-	       [0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1],  # 3
-	       [0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0],  # 4
-	       [1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1],  # 5
-	       [1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1]]) # 6
+n = int(sys.argv[1])
+p = int(sys.argv[2])
+q = int(sys.argv[3])
+print 'Running test for (n,p,q) = (',n,',',p,',',q,')'
 
-# this returns a regular H but it is not in systematic form
-[G, newH] = getSystematicGmatrix(H)
+H = regularLDPCcodeConstructor(n,p,q)
+newH = getFullRankHmatrix(H)
+[invT,invPhi,E,A,B,D,bestH,n,k,g]=getParametersForEncoding(newH,100)
 
-# get the systematic form of H
-k = G.shape[0]
-n = G.shape[1]
-m = n - k
-P = G[0:k,k:n]
-I_m = identity(m,int)
-newH = hstack((P.transpose(),I_m))
-s = random_integers(0,1,k).reshape(k,1)
-x = dot(G.transpose(),s) % 2		# codeword
+print '\n\nTest of encoding k =',k,
+print 'randomly generated information bits'
+print 'H.shape:', bestH.shape
+print 'gap:',g
+print 'Codeword size:', bestH.shape[1]
+rate = divide((k*1.0),bestH.shape[1])
+print 'Rate:','%2.3F' % rate
+maxIterationsLimit = 100
 
-myfile = open('DecodingTestOutput_12_3_4.txt','w')
-maxIterationsLimit = 20
-numBits = x.shape[0]
-tempstring = 'Testing codeword length: ' + `numBits`
-myfile.write(tempstring)
-myfile.write('\n\nFlipping bits\tDecoded Correctly?\n')
+passCount = failCount = 0
+for testNum in arange(10):
+	print '\nTest number:', testNum
 
-# Flipping just one bit first
-totalCount = 0
-correctlyDecoded = 0
-notCorrectlyDecoded = 0
-transmittedCodeword = x.copy().astype(int)
-for index in arange(numBits):
-	totalCount = totalCount + 1
-	tempstring = `index`
-	myfile.write(tempstring)
-	receivedCodeword = transmittedCodeword.copy()
-	receivedCodeword[index] =\
-	             logical_xor(transmittedCodeword[index],1)
-	correctedCodeword = \
-	    bitFlipDecoder(maxIterationsLimit, newH, receivedCodeword)
-	if allclose(correctedCodeword,transmittedCodeword):
-		myfile.write('\t\tyes\t')
-		correctlyDecoded = correctlyDecoded + 1
-	else:
-		myfile.write('\t\tNO\t')
-		notCorrectlyDecoded = notCorrectlyDecoded + 1
-	tempstring = `transmittedCodeword.transpose()`
-	myfile.write(tempstring)
-	myfile.write('<-- transmitted\n\t\t\t')
-	tempstring = `receivedCodeword.transpose()`
-	myfile.write(tempstring)
-	myfile.write('<-- received\n\t\t\t')
-	tempstring = `correctedCodeword.transpose()`
-	myfile.write(tempstring)
-	myfile.write('<-- corrected codeword\n\n')
-tempstring = 'Tests ran with 1 bit flipped: ' + `totalCount` + '\nCorrectly decoded: ' + `correctlyDecoded` + ', Not correctly decoded: ' + `notCorrectlyDecoded` + '\n\n'
-myfile.write(tempstring)
+	s = random_integers(0,1,k).reshape(k,1)
+	x = RichardsonUrbankeEncoder(invT,invPhi,E,A,B,D,bestH,n,k,g,s)
+	numBits = x.shape[0]
+	transmittedCodeword = x.copy()
+	
+	# verify:
+	testArray = dot(bestH,x) % 2
+	
+	# so many rounding errors! I'm seeing 2.0000000 in testArray 
+	# after doing % 2
+	for index in arange(testArray.shape[0]):
+		if (abs(2-testArray[index,0])) < 0.01:
+			# this is a 2, and 2%2 = 0
+			testArray[index,0] = 0
+		elif (abs(1-testArray[index,0])) < 0.01:
+			# this is a 1
+			testArray[index,0] = 1
+		elif (abs(0-testArray[index,0])) < 0.01:
+			# this is a 0
+			testArray[index,0] = 0
+		else: 
+			print 'What value is this???', testArray[index,0]
 
-# flipping 2 bits
-myfile.write('\n\nFlipping bits\tDecoded Correctly?\n')
-totalCount = 0
-correctlyDecoded = 0
-notCorrectlyDecoded = 0
-for index1 in arange(numBits):
-	for index2 in arange(index1+1,numBits):
-		if index1 == index2: continue
-		totalCount = totalCount + 1
-		tempstring = `index1` + '  ' + `index2`
-		myfile.write(tempstring)
-		receivedCodeword = transmittedCodeword.copy()
-		receivedCodeword[index1] =\
-	             logical_xor(transmittedCodeword[index1],1)
-		receivedCodeword[index2] =\
-	             logical_xor(transmittedCodeword[index2],1)          
-		correctedCodeword = \
-	    bitFlipDecoder(maxIterationsLimit, newH, receivedCodeword)
-		if allclose(correctedCodeword,transmittedCodeword):
-			myfile.write('\t\tyes\t')
-			correctlyDecoded = correctlyDecoded + 1
-		else:
-			myfile.write('\t\tNO\t')
-			notCorrectlyDecoded = notCorrectlyDecoded + 1
-		tempstring = `transmittedCodeword.transpose()`
-		myfile.write(tempstring)
-		myfile.write('<-- transmitted\n\t\t\t')
-		tempstring = `receivedCodeword.transpose()`
-		myfile.write(tempstring)
-		myfile.write('<-- received\n\t\t\t')
-		tempstring = `correctedCodeword.transpose()`
-		myfile.write(tempstring)
-		myfile.write('<-- corrected codeword\n\n')
-tempstring = 'Tests ran with 2 bits flipped: ' + `totalCount` + '\nCorrectly decoded: ' + `correctlyDecoded` + ', Not correctly decoded: ' + `notCorrectlyDecoded`
-myfile.write(tempstring)
+	if testArray.any():
+		failCount += 1
+	else: 
+		# if codeword passed parity check, perform bit flip decoding
+		# tests
+		passCount += 1
+		
+		totalCount = 0
+		correctlyDecoded = 0
+		notCorrectlyDecoded = 0
+		for index in arange(numBits):
+			totalCount += 1
+			receivedCodeword = transmittedCodeword.copy()
+			receivedCodeword[index] =\
+			                logical_xor(transmittedCodeword[index],1)
+			correctedCodeword =\
+		bitFlipDecoder(maxIterationsLimit, bestH,receivedCodeword)
+			if allclose(correctedCodeword,transmittedCodeword):
+				correctlyDecoded += 1
+			else:
+				notCorrectlyDecoded += 1
+		successRate = divide(correctlyDecoded, totalCount) * 100
+		print 'Tested flipping every bit, total:',totalCount,
+		print '\nCorrectly decoded:',correctlyDecoded,
+		print '/ Not correctly decoded:',notCorrectlyDecoded,
+		print '/ Success rate:','%2.3F'% successRate
 
-myfile.close()
+		totalCount = 0
+		correctlyDecoded = 0
+		notCorrectlyDecoded = 0
+		# I think 10000 is a sufficent number of tests...
+		while totalCount < 10000: 
+			index1 = randint(0,numBits-1)
+			index2 = randint(0,numBits-1)
+			while index2 == index1:
+				index2 = randint(0,numBits-1)
+			totalCount += 1
+			receivedCodeword = transmittedCodeword.copy()
+			receivedCodeword[index1] =\
+				           logical_xor(transmittedCodeword[index1],1)
+			receivedCodeword[index2] =\
+ 				          logical_xor(transmittedCodeword[index2],1)
+			correctedCodeword = \
+			bitFlipDecoder(maxIterationsLimit,bestH,receivedCodeword)
+			if allclose(correctedCodeword,transmittedCodeword):
+				correctlyDecoded += 1
+			else:
+				notCorrectlyDecoded += 1
+			
+			successRate=divide(correctlyDecoded*1.0, totalCount)*100
+		print 'Tests ran with 2 bits flipped:',totalCount,
+		print '\nCorrectly decoded:',correctlyDecoded,
+		print '/ Not correctly decoded:',notCorrectlyDecoded,
+		print '/ Success rate:','%2.3F'% successRate
+
+print passCount, 'encoding parity check tests passed and', 
+print failCount, 'tests failed.'
