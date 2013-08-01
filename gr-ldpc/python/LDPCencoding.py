2a03333f6cfb33b54e717283ec9dc97931b7dd60
Tracie Perez <tracie.perez@mavs.uta.edu>
2013-07-31 20:07:57 -0500

    Adding block code for Richardson_Urbanke_encoder_ss.
    Functionality moved out of the file LDPCencoding.py and into the work function in the encoder block.

--- gr-ldpc/python/LDPCencoding.py
+++ gr-ldpc/python/LDPCencoding.py
@@ -20,25 +20,4 @@ def matrixMultiplierEncoder(G,s):
 	t = dot(G.transpose(),s) % 2
 
 	return t
-
-def RichardsonUrbankeEncoder(invT,invPhi,E,A,B,D,bestH,n,k,g,s):
-	# This method is from Richardson and Urbanke's Modern Coding
-	# Theory, Appendix A
-
-	# compute p1 and p2 (this method has lowest complexity)
-	a = dot(B,s) % 2
-	b = dot(invT,a) % 2
-	c = dot(E,b) % 2
-	d = dot(D,s) % 2
-	e = d + c % 2
-	p2 = dot(invPhi,e) % 2
-	a = dot(A, p2) % 2
-	a.resize((n-k-g),1)
-	b = dot(B, s) % 2
-	c = a + b % 2
-	p1 = dot(invT,c) % 2
-
-	# concatenate to get codeword x
-	x = vstack((p1, p2, s))
-
-	return x
+	
--- gr-ldpc/python/Richardson_Urbanke_encoder_ss.py
+++ gr-ldpc/python/Richardson_Urbanke_encoder_ss.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # 
-# Copyright 2013 <+YOU OR YOUR COMPANY+>.
+# Copyright 2013 Tracie Perez
 # 
 # This is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,24 +18,57 @@
 # Boston, MA 02110-1301, USA.
 # 
 
-import numpy
+import numpy as np
 from gnuradio import gr
 
 class Richardson_Urbanke_encoder_ss(gr.sync_block):
     """
-    docstring for block Richardson_Urbanke_encoder_ss
+    This encoding method is from Richardson and Urbanke's Modern 
+    Coding Theory, Appendix A. Arguments to the constructor 
+    should be taken care of during a preprocessing step using the 
+    function getParametersForEncoding so that the speed of real-time 
+    encoding is minimized. 
     """
-    def __init__(self):
+    def __init__(self,invT,invPhi,E,A,B,D,n,k,g):
+        self.inverseT   = invT
+        self.inversePhi = invPhi
+        self.E = E
+        self.A = A
+        self.B = B
+        self.D = D
+        self.n = n
+        self.k = k
+        self.gap = g
         gr.sync_block.__init__(self,
             name="Richardson_Urbanke_encoder_ss",
-            in_sig=[<+numpy.float+>],
-            out_sig=[<+numpy.float+>])
-
+            in_sig=[(np.int32,self.k)],
+            out_sig=[(np.int32,self.n)])
 
     def work(self, input_items, output_items):
-        in0 = input_items[0]
-        out = output_items[0]
-        # <+signal processing here+>
-        out[:] = in0
-        return len(output_items[0])
+        # s is the word of k information bits to be encoded
+        s = np.zeros((self.k,1), dtype=int)
+        for i in np.arange(len(input_items[0][0])):
+            s[i,0] = input_items[0][0][i]
 
+        # compute p1 and p2 (this method has lowest complexity)
+        a = np.dot(self.B, s) % 2
+        b = np.dot(self.inverseT, a) % 2
+        c = np.dot(self.E, b) % 2
+        d = np.dot(self.D, s) % 2
+        e = d + c % 2
+        p2 = np.dot(self.inversePhi, e) % 2
+        a = np.dot(self.A, p2) % 2
+        a.resize((self.n-self.k-self.gap), 1)
+        b = np.dot(self.B, s) % 2
+        c = a + b % 2
+        p1 = np.dot(self.inverseT, c) % 2
+
+        # concatenate to get codeword
+        codeword = np.vstack((p1, p2, s))
+
+        # FIXME use the slice operator when assigning to output items
+        # out[:] = in0
+
+        for i in np.arange(len(output_items[0][0])):
+            output_items[0][0][i] = codeword[i,0]
+        return len(output_items[0])
