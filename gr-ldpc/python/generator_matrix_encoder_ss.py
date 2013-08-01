e9796e29e88a070577402fc7b1932c2a8c9b82c2
Tracie Perez <tracie.perez@mavs.uta.edu>
2013-08-01 11:29:42 -0500

    Adding block code to generator_matrix_encoder_ss.
    This is a straightforward encoding block for creating a codeword by multiplying the generator matrix G by the source word s.

--- gr-ldpc/python/generator_matrix_encoder_ss.py
+++ gr-ldpc/python/generator_matrix_encoder_ss.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # 
-# Copyright 2013 <+YOU OR YOUR COMPANY+>.
+# Copyright 2013 Tracie Perez.
 # 
 # This is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,24 +18,46 @@
 # Boston, MA 02110-1301, USA.
 # 
 
-import numpy
+import numpy as np
 from gnuradio import gr
 
 class generator_matrix_encoder_ss(gr.sync_block):
     """
-    docstring for block generator_matrix_encoder_ss
+    This is a standard encoding method that is discussed in many
+    error-correcting textbooks. One is: Turbo Coding for Satellite
+    and Wireless Communications by Soleymani, Gao, and 
+    Vilaipornsawai. 
+
+    The generator matrix G must be in systematic
+    form. Use function getSystematicGmatrix if necessary.
     """
-    def __init__(self):
+    def __init__(self,G):
+        self.G = G
+        self.k = G.shape[0]
+        self.n = G.shape[1]
+        if np.any(G[0:k,0:k]-np.identity(k)):
+            print 'Error: G not in systematic form.'
         gr.sync_block.__init__(self,
             name="generator_matrix_encoder_ss",
-            in_sig=[<+numpy.float+>],
-            out_sig=[<+numpy.float+>])
-
+            in_sig=[(np.int32,self.k)],
+            out_sig=[(np.int32,self.n)])
 
     def work(self, input_items, output_items):
-        in0 = input_items[0]
-        out = output_items[0]
-        # <+signal processing here+>
-        out[:] = in0
-        return len(output_items[0])
+        # s is the word of k information bits to be encoded
+        s = np.zeros((self.k,1), dtype=int)
+        for i in np.arange(len(input_items[0][0])):
+            s[i,0] = input_items[0][0][i]
 
+        if self.k != s.shape[0]:
+            print 'Error: provided message is not the right size.'
+            return
+        
+        codeword = np.dot(G.transpose(),s) % 2
+
+        # FIXME use the slice operator when assigning to output items
+        # out[:] = in0
+
+        for i in np.arange(len(output_items[0][0])):
+            output_items[0][0][i] = codeword[i,0]        
+
+        return len(output_items[0])
