8b8dbd8c311965636b5609d2104311b2af00cf17
Tracie Perez <tracie.perez@mavs.uta.edu>
2013-08-02 11:31:49 -0500

    Moving function writeAlistFile to LDPC class.
    Moving the function writeAlistFile from the file LDPChelperFunctions.py into LDPC_parity_check_matrix class. Deleting LDPChelperFunctions.py since all functions have now been moved out of this file.

--- gr-ldpc/python/LDPC_H_matrix.py
+++ gr-ldpc/python/LDPC_H_matrix.py
@@ -60,6 +60,63 @@ class LDPC_parity_check_matrix:
 
 		return H
 
+	def writeAlistFile(filename,H,verbose=0):
+		# This function reads in a parity check matrix H and writes 
+		# the corresponding alist file. The format of alist files is
+		# desribed at: 
+		# http://www.inference.phy.cam.ac.uk/mackay/codes/alist.html
+
+		myfile = open(filename,'w')
+
+		numRows = H.shape[0]
+		numCols = H.shape[1]
+
+		tempstring = `numCols` + ' ' + `numRows` + '\n'
+		myfile.write(tempstring)
+
+		tempstring1 = ''
+		tempstring2 = ''
+		maxRowWeight = 0
+		for rowNum in np.arange(numRows):
+			nonzeros = np.array(H[rowNum,:].nonzero())
+			rowWeight = nonzeros.shape[1]
+			if rowWeight > maxRowWeight:
+				maxRowWeight = rowWeight
+			tempstring1 = tempstring1 + `rowWeight` + ' '
+			for tempArray in nonzeros:
+				for index in tempArray:
+					tempstring2 = tempstring2 + `index+1` + ' '
+				tempstring2 = tempstring2 + '\n'
+		tempstring1 = tempstring1 + '\n'
+
+		tempstring3 = ''
+		tempstring4 = ''
+		maxColWeight = 0
+		for colNum in np.arange(numCols):
+			nonzeros = np.array(H[:,colNum].nonzero())
+			colWeight = nonzeros.shape[1]
+			if colWeight > maxColWeight:
+				maxColWeight = colWeight
+			tempstring3 = tempstring3 + `colWeight` + ' '
+			for tempArray in nonzeros:
+				for index in tempArray:
+					tempstring4 = tempstring4 + `index+1` + ' '
+				tempstring4 = tempstring4 + '\n'
+		tempstring3 = tempstring3 + '\n'
+
+		tempstring = `maxColWeight` + ' ' + `maxRowWeight` + '\n'
+		# write out max column and row weights
+		myfile.write(tempstring)
+		# write out all of the column weights
+		myfile.write(tempstring3) 
+		# write out all of the row weights
+		myfile.write(tempstring1) 
+		# write out the nonzero indices for each column
+		myfile.write(tempstring4) 
+		# write out the nonzero indices for each row
+		myfile.write(tempstring2) 
+		# close the file
+		myfile.close()
 
 	def regular_LDPC_code_contructor(self,n_p_q):
 
